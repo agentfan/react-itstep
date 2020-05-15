318b04a72c2248aaae8a3afa21a4b192e4b3f392
agentfan <agentfan@gmail.com>
2020-05-15 20:47:58 +0300

    changed import

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 import {name7} from "./_help.js"
 
-let student = {
-    name: "Victor",
-    surname: "Nagorniy",
-    age: 42
-};
-
-// let {name, surname, age} = student;
-
 let student1 = {
     name: "Victor",
     surname: "Nagorniy",
