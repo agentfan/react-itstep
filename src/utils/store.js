c40ef17700d8ba0208f67c95c82b9fa84d0ebc5d
agentfan <agentfan@gmail.com>
2020-06-10 21:44:37 +0300

    added images

--- src/utils/store.js
+++ src/utils/store.js
@@ -2,34 +2,46 @@ import React, { useState } from 'react';
 
 export const StoreContext = React.createContext(null);
 
-const Store = ({ children }) => {
-    const infoOfPainter = {
-        name: "",
-        surname: "",
-        img: "",
-        notes: "",
-        periods: [
-            {
-                id: 0,
-                title: "",
-                notes: "",
-                works: [
-                    {
-                        id: 0,
-                        title: "",
-                        year: 1897,
-                        img: '',
-                        notes: "",
-                        likes: 0
-                    },
-                ]
-            },
-        ]
-    };
+const infoOfPainter = {
+    name: "",
+    surname: "",
+    img: "",
+    notes: [
+        {
+            id: 0,
+            text: ""
+        }
+    ],
+    periods: [
+        {
+            id: 0,
+            title: "",
+            notes: [
+                {
+                    id: 0,
+                    text: ""
+                }
+            ],
+            works: [
+                {
+                    id: 0,
+                    title: "",
+                    year: 1897,
+                    img: '',
+                    notes: "",
+                    likes: 0
+                },
+            ]
+        },
+    ]
+};
 
+const Store = ({ children }) => {
     const [painter, setPainter] = useState(infoOfPainter);
 
-    const store = {painter, setPainter};
+    const store = {
+        painter: painter, setPainter
+    };
 
     return (
         <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
